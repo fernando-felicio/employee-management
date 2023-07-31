5bc11179879d031ab63c35162673c4b95c4fa9ff
Fernando Felício <fernandofelicio.dev@gmail.com>
2023-07-30 23:45:45 -0300

    Criando entities e chamando metodo netSalary

--- src/entities/Employee.java
+++ src/entities/Employee.java
@@ -1,5 +1,19 @@
 package entities;
 
 public class Employee {
+	
+	//Declarando atributos da classe Employee
+	String name;
+	double grossSalary;
+	double tax;
+	
+	//Criando método netSalary
+	
+	public double netSalary() {
+		double netSalary = grossSalary - tax;
+		return netSalary;
+	}
+	
+	
 
 }
